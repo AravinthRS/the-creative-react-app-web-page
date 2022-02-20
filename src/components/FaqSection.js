3af9a522ef5d87f26776c25b625c2ad4033edbba
Aravinth RS <aravinthrs1705@gmail.com>
2022-02-20 12:46:37 +0530

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions<span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How I do start</h4>
+        <div className="answer">
+          <p>The sender of this email is registered with Naukri.com</p>
+          <p>
+            The sender of this email is registered with Naukri.com as TESCRA
+            SOFTWARE PRIVATE LIMITED company. To respond back directly to the
+            Employer, please click on Reply button, or send an
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily schedule</h4>
+        <div className="answer">
+          <p>The sender of this email is registered with Naukri.com</p>
+          <p>
+            The sender of this email is registered with Naukri.com as TESCRA
+            SOFTWARE PRIVATE LIMITED company. To respond back directly to the
+            Employer, please click on Reply button, or send an
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different payment method </h4>
+        <div className="answer">
+          <p>The sender of this email is registered with Naukri.com</p>
+          <p>
+            The sender of this email is registered with Naukri.com as TESCRA
+            SOFTWARE PRIVATE LIMITED company. To respond back directly to the
+            Employer, please click on Reply button, or send an
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What product do you offer</h4>
+        <div className="answer">
+          <p>The sender of this email is registered with Naukri.com</p>
+          <p>
+            The sender of this email is registered with Naukri.com as TESCRA
+            SOFTWARE PRIVATE LIMITED company. To respond back directly to the
+            Employer, please click on Reply button, or send an
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
